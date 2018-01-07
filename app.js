425262561c04a100ee819295f761f43e81d9859e
Matt Ackard <mattackard@gmail.com>
2018-01-06 23:31:09 -0800

    smoother transition between pages

--- app.js
+++ app.js
@@ -8,38 +8,48 @@ $(".pageLink").click(function () {
     "use strict";
     
     if ($(this).hasClass("webLink")) {
-        $("#bigNav").fadeOut();
-        $(".subPage").fadeOut();
-        $("#webPage").load("webdev.html");
-        $("#webPage").css("display", "flex");
+        $("#bigNav").fadeOut(function () {
+            $(".subPage").fadeOut(function() {
+                $("#webPage").load("webdev.html", function () {
+                    $("#webPage").css("display", "flex").hide().fadeIn();
+                });
+            });
+        });
     }
     
     else if ($(this).hasClass("climbLink")) {
-        $("#bigNav").fadeOut();
-        $(".subPage").fadeOut();
-        $("#climmbPage").load("climb.html");
-        $("#climbPage").css("display", "flex");
+        $("#bigNav").fadeOut(function () {
+            $(".subPage").fadeOut(function() {
+                $("#climbPage").load("climb.html", function () {
+                    $("#climbPage").css("display", "flex").hide().fadeIn();
+                });
+            });
+        });
     }
     
     else if ($(this).hasClass("musicLink")) {
-        $("#bigNav").fadeOut();
-        $(".subPage").fadeOut();
-        $("#musicPage").load("music.html");
-        $("#musicPage").css("display", "flex");
+        $("#bigNav").fadeOut(function () {
+            $(".subPage").fadeOut(function() {
+                $("#musicPage").load("music.html", function () {
+                    $("#musicPage").css("display", "flex").hide().fadeIn();
+                });
+            });
+        });
     }
     
     else if ($(this).hasClass("contactLink")) {
-        console.log("clicked contact");
-        $("#bigNav").fadeOut();
-        $(".subPage").fadeOut();
-        $("#contactPage").load("contact.html");
-        $("#contactPage").css("display", "flex");
+        $("#bigNav").fadeOut(function () {
+            $(".subPage").fadeOut(function() {
+                $("#contactPage").load("contact.html", function () {
+                    $("#contactPage").css("display", "flex").hide().fadeIn();
+                });
+            });
+        });
     }
     
     else {
-        console.log("clicked main header");
-        $(".subPage").fadeOut();
-        $("#bigNav").css("display", "flex");
-        $("#bigNav button").fadeIn();
+        $(".subPage").fadeOut(function() {  
+            $("#bigNav").css("display", "flex").hide().fadeIn();   
+        });       
     }
 });
