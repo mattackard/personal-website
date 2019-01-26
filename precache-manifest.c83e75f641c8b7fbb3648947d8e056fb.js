051adb6a3fd343465bd3f0f3ec5743846038072e
Matthew Ackard <mattackard@gmail.com>
2019-01-25 19:46:51 -0800

    Updates

---
+++ precache-manifest.c83e75f641c8b7fbb3648947d8e056fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c70667ee42c60d4a4745",
+    "url": "/static/js/main.c70667ee.chunk.js"
+  },
+  {
+    "revision": "46a5bd547a95e547f122",
+    "url": "/static/js/1.46a5bd54.chunk.js"
+  },
+  {
+    "revision": "c70667ee42c60d4a4745",
+    "url": "/static/css/main.135ed44d.chunk.css"
+  },
+  {
+    "revision": "4e2492950ef25e45a37deb4add6d4c0a",
+    "url": "/index.html"
+  }
+];
