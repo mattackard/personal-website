94773079ffa66ab1044b27be2ddb8f04122ff152
Matt Ackard <mattackard@gmail.com>
2019-03-14 20:30:34 -0700

    favicon update and edited bio text

--- src/components/Webdev.js
+++ src/components/Webdev.js
@@ -9,16 +9,15 @@ class Webdev extends Component {
 				<h1>Web Development</h1>
 				<p>
 					I am a web developer from San Jose, CA. I am a self-taught
-					developer focused on Javascript with React. I have
-					experience building apps with Node, Epxpress, and Mongo as
-					well. I enjoy working on interactive and media-heavy sites
-					as well as single page apps with React. My portfolio is
-					below with short descriptions for each project along with
-					some of the specific technologies used. Live links as well
-					as links to the source for each portfolio project are
-					provided. My email and links to my LinkedIn and Github
-					profiles are at the top of the page. Thanks for stopping by
-					to look at my work!
+					developer focused on Javascript with the MERN stack. I enjoy
+					working on interactive and media-heavy sites as well as
+					single page apps with React. My portfolio is below with
+					short descriptions for each project along with some of the
+					specific tech used. Live links as well as links to the
+					github source for each portfolio project are provided. My
+					email and links to my LinkedIn and Github profiles are at
+					the top of the page. Thanks for stopping by to look at my
+					work!
 				</p>
 				<div id="webProjects">
 					<h1>My projects</h1>
